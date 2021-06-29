33011a8366492ed81bf352d9a2e0ec1d1f18d109
eighty88 <49605393+eighty88@users.noreply.github.com>
2021-06-29 19:26:01 +0900

    Update JsonObject.java

--- src/main/java/si/f5/pa_union/JsonObject.java
+++ src/main/java/si/f5/pa_union/JsonObject.java
@@ -3,22 +3,27 @@ package si.f5.pa_union;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class JsonObject {
 
     String value;
 
+    HashMap<String, String> hashMap = null;
+    List<String> arrayList = null;
+
     public JsonObject(String[] strings) {
         StringBuilder builder = new StringBuilder();
         for(String str: strings) {
             builder.append(str);
         }
-        value = builder.toString();
+        init(builder.toString());
     }
 
     public JsonObject(String string) {
-        value = string;
+        init(string);
     }
 
     public JsonObject(List<String> strings) {
@@ -26,7 +31,7 @@ public class JsonObject {
         for(String str: strings) {
             builder.append(str);
         }
-        value = builder.toString();
+        init(builder.toString());
     }
 
     public JsonObject(InputStreamReader InputReader) throws IOException {
@@ -36,36 +41,11 @@ public class JsonObject {
         while ((str = reader.readLine()) != null) {
             builder.append(str);
         }
-        value = builder.toString();
+        init(builder.toString());
     }
 
     public JsonObject get(String str) {
-        if(value.contains(str)) {
-            String tempString = value.split("\"" + str + "\"", 2)[1].replace("\\\"", "${__double_q}");
-            int tempInt = 0;
-            int q = 0;
-
-            StringBuilder builder = new StringBuilder();
-            for(char chr: tempString.toCharArray()) {
-                if(tempInt == 0) {
-                    if (chr == '}' || chr == ']' || chr == ',')
-                        return new JsonObject(builder.toString().replaceFirst(":", "").replace("${__double_q}", "\\\""));
-                }
-                if (chr == '}' || chr == ']') {
-                    tempInt--;
-                }
-                if (chr == '{' || chr == '[') {
-                    tempInt++;
-                }
-                if (chr == '"') {
-                    q = 1 - q;
-                }
-                if(q == 0 && chr != ' ') {
-                    builder.append(chr);
-                }
-            }
-        }
-        return null;
+        return new JsonObject(hashMap.get(str));
     }
 
     public String asString() {
@@ -77,11 +57,79 @@ public class JsonObject {
     }
 
     public List<JsonObject> asArray() {
-        return null; //Todo create
+        List<JsonObject> result = new ArrayList<>();
+        for(String str: arrayList) result.add(new JsonObject(str));
+        return result;
     }
 
     @Override
     public String toString() {
-        return value;
+        if (arrayList != null) {
+            StringBuilder builder = new StringBuilder("[\n");
+            for (String str : arrayList) {
+                builder.append("\t")
+                        .append(new JsonObject(str)).append("\n");
+            }
+            builder.append("]");
+            return builder.toString();
+        } else if (hashMap != null) {
+            int TempInt = 1;
+            StringBuilder builder = new StringBuilder("{\n");
+            for(String key: hashMap.keySet()) {
+                builder.append("\t").append(key).append(": ").append(new JsonObject(hashMap.get(key)).toString().replace("\n", "\n\t")).append(",").append("\n");
+            }
+            builder.append("}");
+            return builder.toString().replaceFirst(",\n}$", "\n}");
+        } else if (value != null) {
+            return value;
+        }
+        return null;
+    }
+
+    private void init(String v) {
+        String temp = v.replace("\\\"", "${__double_q}").replaceAll("\\s+","");
+        temp = EraseSpace(temp);
+        boolean list = false;
+
+        if(temp.startsWith("[")) {
+            list = true;
+            arrayList = new ArrayList<>();
+        } else if (temp.startsWith("{")){
+            hashMap = new HashMap<>();
+        } else {
+            value = temp;
+            return;
+        }
+
+        StringBuilder builder = new StringBuilder();
+        int tempInt = 0;
+        boolean b = false;
+        for(char chr: temp.toCharArray()) {
+            if(chr == '"') b = !b;
+            if(tempInt == 1) {
+                if (chr == '}' || chr == ']' || chr == ',') {
+                    if (list) {
+                        arrayList.add(builder.toString());
+                    } else {
+                        hashMap.put(builder.toString().split(":", 2)[0], builder.toString().split(":", 2)[1]);
+                    }
+                    builder = new StringBuilder();
+                    continue;
+                }
+            }
+            if (chr == '}' || chr == ']') tempInt--;
+            if (chr == '{' || chr == '[') tempInt++;
+            if(!b) builder.append(chr);
+        }
+    }
+
+    private String EraseSpace(String v) {
+        StringBuilder builder = new StringBuilder();
+        boolean b = false;
+        for(char chr: v.toCharArray()) {
+            if(chr == '"') b = !b;
+            if(chr != ' ' && b) builder.append(chr);
+        }
+        return builder.toString();
     }
 }
